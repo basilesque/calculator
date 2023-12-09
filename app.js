9b5e7a9bb34ffb28ea7c75d12d409fe928d139cb
Reyhan <reyhantoron@gmail.com>
2023-12-09 12:21:21 +0300

    removed comments

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-//hesap makinesinin ekranının elementini bununla seçiyoruz
 const input = document.getElementById('inputtext');
 
-//tüm butonların elementlerini bununla seçiyoruz
 const buttons = document.querySelectorAll('button');
 
-//boş butona ve ekrana gelince imlecin görüntüsünün değişmemesi için
+
 document.getElementById("empty").style.cursor = "default";
 document.getElementById("inputtext").style.cursor = "default";
 
 document.getElementById("inputtext").disabled = true;
 
-//hesap yapan fonksiyon
 function calculate(expression) {
-    //console.log(expression);
-    //console.log(typeof (expression));
     try {
         return new Function('return ' + expression)();
     } catch (error) {
@@ -21,7 +16,6 @@ function calculate(expression) {
     }
 }
 
-//C'ye basınca siliyor, DEL'e basınca bir geri gidiyor, sayıya basınca ekrana yazıyor ve eşittire basınca ise calcute fonskiyonunu çağırıyor
 function operation(buttonValue) {
     if (buttonValue === 'C') {
         input.value = '';
@@ -34,7 +28,6 @@ function operation(buttonValue) {
     }
 }
 
-//butonların değeri (value) içlerindeki sayı olacak, her tıklanışta operation fonksiyonunu çağırıyor aynı zamanda
 buttons.forEach(button => {
     let buttonValue = button.innerText;
     button.addEventListener('click', function () {
